dc81f7237dc86a37f5a4ca1513fb20150da67b72
Diegomca98 <diegomca98@gmail.com>
2021-04-16 20:37:22 -0500

    Authentication in Django

--- foodcatalog/foodcatalog/urls.py
+++ foodcatalog/foodcatalog/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 import food.views as fv
+from Users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', fv.index, name="index"),
-    path('food/', include('food.urls'))
-]
+    path('food/', include('food.urls')),
+    path('register/', user_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
+    path('profile/', user_views.profilepage, name="profile")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
